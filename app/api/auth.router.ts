2dcdc40c979895ddf3ab6af6b750b30f530fc447
nik-kita <nik@kita.github>
2025-06-18 05:11:43 +0200

    use plugin_config in auth_router (in progress)

--- app/api/auth.router.ts
+++ app/api/auth.router.ts
@@ -1,20 +1,21 @@
-import { Elysia } from "elysia";
 import {
     gen__sign_in_url,
     GOOGLE_EMAIL_SCOPE,
     GOOGLE_OPEN_ID_SCOPE,
     init__oauth2_client,
 } from "@lib/google";
+import { Elysia } from "elysia";
+import { plugin_config } from "./plugin_config.ts";
 
 export const auth_router = new Elysia()
-    .decorate(
-        "gOauth",
-        init__oauth2_client({
-            client_id: "TODO",
-            client_secret: "TODO",
-            redirect_url: "TODO",
+    .use(plugin_config)
+    .derive(({ config }) => ({
+        gOauth: init__oauth2_client({
+            client_id: config.TODO,
+            client_secret: config.TODO,
+            redirect_url: config.TODO,
         }),
-    )
+    }))
     .get("/google-email", ({ redirect, gOauth }) => {
         const url = gen__sign_in_url(gOauth, {
             scope: [
