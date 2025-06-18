0da2607a24be34a10c58d7622c409810c54e887f
nik-kita <nik@kita.github>
2025-06-18 05:57:09 +0200

    lib/config => init__config()  /// utilize Effect Schema

--- lib/config/mod.lib.config.ts
+++ lib/config/mod.lib.config.ts
@@ -1,10 +1,21 @@
-import { Schema as S } from "effect";
+import { load } from "@std/dotenv";
+import { Effect, Schema as S } from "effect";
 
 export const EnvSchema = S.Struct({
   GOOGLE_CLIENT_ID: S.String,
   GOOGLE_CLIENT_SECRET: S.String,
+  API_HOST: S.String,
 });
 
-export const config = {
-  TODO: "todo",
-} as const;
+export const init__config = Effect.gen(function* () {
+  const raw = yield* Effect.tryPromise(() => load());
+  const config = yield* S.decodeUnknown(EnvSchema)(raw);
+
+  return {
+    GOOGLE: {
+      client_id: config.GOOGLE_CLIENT_ID,
+      secret: config.GOOGLE_CLIENT_SECRET,
+    },
+    API_HOST: config.API_HOST,
+  };
+}).pipe(Effect.orDie);
