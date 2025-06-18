baa50deb4874dd1e1c8f485799492dbcd4d2bf5c
nik-kita <nik@kita.github>
2025-06-18 04:17:38 +0200

    lib/google => update with some constants

--- app/api/auth.router.ts
+++ app/api/auth.router.ts
@@ -1,5 +1,5 @@
 import { Elysia } from "elysia";
 
 export const auth_router = new Elysia()
-    .post("/google-email", () => {
+    .get("/google-email", () => {
     });
--- lib/google/mod.lib.google.ts
+++ lib/google/mod.lib.google.ts
@@ -1,3 +1,4 @@
 export { gen__sign_in_url } from "./gen__sign_in_url.ts";
 export { init__oauth2_client } from "./init__oauth2_client.ts";
 export { parse__token_from_db_res } from "./parse__token_from_cb_res.ts";
+export * from "./const.ts";
