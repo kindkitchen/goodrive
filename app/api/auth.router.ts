013a078e8f31fa9fb457f3930d7c7d9689c35f57
nik-kita <nik@kita.github>
2025-06-18 04:24:16 +0200

    draft /google-email handler

--- app/api/auth.router.ts
+++ app/api/auth.router.ts
@@ -1,5 +1,28 @@
 import { Elysia } from "elysia";
+import {
+    gen__sign_in_url,
+    GOOGLE_EMAIL_SCOPE,
+    GOOGLE_OPEN_ID_SCOPE,
+    init__oauth2_client,
+} from "@lib/google";
 
 export const auth_router = new Elysia()
-    .get("/google-email", () => {
+    .decorate(
+        "gOauth",
+        init__oauth2_client({
+            client_id: "TODO",
+            client_secret: "TODO",
+            redirect_url: "TODO",
+        }),
+    )
+    .get("/google-email", ({ redirect, gOauth }) => {
+        const url = gen__sign_in_url(gOauth, {
+            scope: [
+                GOOGLE_EMAIL_SCOPE,
+                GOOGLE_OPEN_ID_SCOPE,
+            ],
+            state: "TODO",
+        });
+
+        return redirect(url);
     });
