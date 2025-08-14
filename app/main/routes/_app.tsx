e9a31b7c9e4c2f76b3ae49ef750e9745722c47c6
nik-kita <nik@kita.github>
2025-08-14 23:13:54 +0200

    deno run -Ar jsr:@fresh/init@2.0.0-alpha.58

---
+++ app/main/routes/_app.tsx
@@ -0,0 +1,17 @@
+import { define } from "../utils.ts";
+
+export default define.page(function App({ Component, state }) {
+  return (
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{state.title ?? "main"}</title>
+        <link rel="stylesheet" href="/styles.css" />
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+});
