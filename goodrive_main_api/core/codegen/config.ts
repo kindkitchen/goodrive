de158673ffa99ad612353f66a298b3c4802e0ffb
nik-kita <nik@kita.github>
2025-08-15 15:00:10 +0200

    reorder codegen stuff

---
+++ goodrive_main_api/core/codegen/config.ts
@@ -0,0 +1,25 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: ["../../type_defs/**/*.gql"],
+  generates: {
+    "./core/codegen/type_defs_out.ts": {
+      plugins: ["typescript", "typescript-operations", "typescript-resolvers"],
+      config: {
+        contextType: "./core/codegen/GqlApiCtx.ts#GqlApiCtx",
+        mappers: {},
+        enumPrefix: true,
+        enumsAsTypes: true,
+        showUnusedMappers: true,
+      },
+    },
+    "./core/codegen/schema.gql": {
+      plugins: ["schema-ast"],
+      config: {
+        includeDirectives: true,
+      },
+    },
+  },
+  emitLegacyCommonJSImports: false,
+};
+export default config;
